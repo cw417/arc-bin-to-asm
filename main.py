7c33544eb3c4130e4536152fd31b20fd635cdb15
cw417 <chriswhitlock417@gmail.com>
2022-02-10 22:45:49 -0500

    Finish adding logic ops; asm formatting  refactor

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def parse_binary(num):
   imm = num[18]
   simm13 = int(num[19:], 2)
 
+  imm_0 = f" {rs1}, {rs2}, {rd}"
+  imm_1 = f" {rs1}, {simm13}, {rd}"
+
   print(f"{op} {rd} {op3} {rs1} {imm} {simm13}")
   #rd_asm = "{0:0>5}".format(str(int(rd, 2)))
   rs1_asm = 'hi'
@@ -28,36 +31,44 @@ def parse_binary(num):
     # addcc
     if op3 == '010000':
       if imm == '0':
-        return f"addcc {rs1}, {rs2}, {rd}"
+        return "addcc" + imm_0
       elif imm == '1':
-        return f"addcc {rs1}, {simm13}, {rd}"
+        return "addcc" + imm_1
     # subcc
     if op3 == '010100':
       if imm == '0':
-        return f"subcc {rs1}, {rs2}, {rd}"
+        return "subcc" + imm_0
       elif imm == '1':
-        return f"subcc {rs1}, {simm13}, {rd}"
+        return "subcc" + imm_1
     #andcc
+    if op3 == '010001':
+      return "andcc" + imm_0
     #orcc
+    if op3 == '010010':
+      return "orcc" +imm_0
     #ornc
+    if op3 == '010110':
+      return "orncc" + imm_0
+    # srl
+    if op3 == '100110':
+      if imm == '0':
+        return "srl" + imm_0
+      elif imm == '1':
+        return "srl" + imm_1
 
   # Memory Functions
   elif op == '11':
     # ld
     if op3 == '000000':
-      asm += f"ld [{int(simm13, 2)}], %r{int(rd, 2)}"
+      return f"ld [{simm13}], {rd}"
     # st
     elif op3 == '001000':
-      asm = f"st %r{int(rd, 2)}, [{int(simm13, 2)}]"
+      return f"st {rd}, [{simm13}]"
   
-
-  return asm
-
-
 def get_info():
   return input('What is the instruction? ')
 
 if __name__ == '__main__':
-  test = '10000000101000010010000000000011'
+  test = '11000010000000000010101110111000'.replace(" ", "")
   #info = get_info()
   print(parse_binary(test))
