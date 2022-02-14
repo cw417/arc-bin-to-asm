0542ddd556dba43e8b7e83ab0bf7e5adc192871c
cw417 <chriswhitlock417@gmail.com>
2022-02-14 17:16:47 -0500

    Finish asm logic

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ def parse_binary(num):
 
   # define instruction part intervals
   op = num[0:2]
+  cond = num[3:7]
   rd = f"%r{int(num[2:7],2)}"
+  op2 = num[7:10]
   op3 = num[7:13]
   rs1 = f"%r{int(num[13:18],2)}"
   rs2 = f"%r{int(num[27:],2)}"
   imm = num[18]
+  imm22 = int(num[11:], 2)
   simm13 = int(num[19:], 2)
+  disp30 = int(num[2:], 2)
+
 
   # format register output based on immediate
   imm_0 = f" {rs1}, {rs2}, {rd}"
@@ -21,13 +26,27 @@ def parse_binary(num):
 
   # Branch functions
   if op == '00':
-    # Need to figure out how to deal with labels
-    pass
+    # branch
+    if op2 == "010":
+      if cond == "0001":
+         b_type = "be"
+      elif cond== "0101":
+        b_type = "bcs"
+      elif cond == "0110":
+        b_type = "bneg"
+      elif cond == "0111":
+        b_type = "bvs"
+      elif cond == "1000":
+        b_type = "ba"
+      return f"{b_type} {imm22}"
+    # sethi
+    if op2 == "100":
+      return f"sethi {imm22}, {rd}"
 
   # Call functions
   elif op == '01':
     # will add call later
-    pass
+    return f"call {disp30}"
 
   # Arithmetic functions
   elif op == '10':
@@ -42,7 +61,7 @@ def parse_binary(num):
       return "andcc" + imm_0
     #orcc
     if op3 == '010010':
-      return "orcc" +imm_0
+      return "orcc" + imm_0
     #ornc
     if op3 == '010110':
       return "orncc" + imm_0
@@ -63,7 +82,7 @@ def get_info():
   return input('What is the instruction? ')
 
 if __name__ == '__main__':
-  test = '11000010000000000010101110111000'
+  test = '000 0110 010 0000000000000000000100'
   #info = get_info()
   #print(parse_binary(info))
   print(parse_binary(test))
